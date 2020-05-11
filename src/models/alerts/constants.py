cc753ca8e173c212d9b4e34df83fecbd14794f64
MarcelFink <marcel-fink@live.de>
2020-05-11 13:50:25 +0200

    Test

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,8 +1,8 @@
 __author__ = 'jslvtr'
 
 
-URL = "https://api.mailgun.net/v3/<>.mailgun.org/messages"
-API_KEY = "key-<>"
-FROM = "Mailgun Sandbox <postmaster@<>.mailgun.org>"
+URL = "https://api.mailgun.net/v3/sandbox0e8763de1aa44068b3290351f3ffdbfa.mailgun.org/messages"
+API_KEY = "c211459fb8d8112065a9c6a44aa401cc-0afbfc6c-d19d4484"
+FROM = "Mailgun Sandbox <postmaster@sandbox0e8763de1aa44068b3290351f3ffdbfa.mailgun.org>"
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
